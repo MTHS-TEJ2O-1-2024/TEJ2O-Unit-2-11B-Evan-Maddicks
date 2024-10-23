09a2a887a81b064bfdbca4f346c76fec70213841
Evan-Maddicks <evan.maddicks@mths.ca>
2024-10-23 11:06:33 -0400

    program now generates 2 random numbers and determines which is greater than the other

--- main.ts
+++ main.ts
@@ -1,8 +1,31 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Evan M
+ * Created on: Oct 2024
+ * This program now generates 2 random numbers and determines which is greater than the other 
 */
 
-basic.showString('Hello, World!')
+let firstnumber = randint(0, 99)
+let secondnumber = randint(0, 99)
+basic.clearScreen()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A,function(){
+basic.showString("#1 : ")
+basic.showNumber(firstnumber)
+basic.clearScreen()
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showString("#2 : ")
+    basic.showNumber(secondnumber)
+    basic.clearScreen()
+})
+
+input.onGesture(Gesture.Shake,function(){
+    if (firstnumber > secondnumber) {
+    basic.showString(firstnumber + ">" + secondnumber)
+} else
+basic.showString(firstnumber + "<" + secondnumber)
+    basic.showIcon(IconNames.Happy)
+})
